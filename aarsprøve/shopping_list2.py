9e114b3d5756b12094c9c0abdd0dc7218e76ad49
B6rnard <emilvandijk@outlook.dk>
2025-03-07 11:18:40 +0100

    s

--- aarsprøve/shopping_list2.py
+++ aarsprøve/shopping_list2.py
@@ -1,38 +1,36 @@
 class ShoppingList:
+   
     def __init__(self):
         self.items = {}  # Skiftet fra en liste til en dictionary
-        self.price_list = {
-            "BOOST": "Booster",
-            "BIGM": "Big Mac Menu",
-            "APPL": "Apples",
-            "BANA": "Bananas"
-        }
 
-    def add_item(self, item_id, quantity=1):
-        if item_id in self.items:
-            self.items[item_id] += quantity
+    def add_item(self, item, quantity=1):
+        if item in self.items:
+            self.items[item] += quantity
         else:
-            self.items[item_id] = quantity
+            self.items[item] = quantity
 
-    def remove_item(self, item_id, quantity=1):
-        if item_id in self.items:
-            self.items[item_id] -= quantity
-            if self.items[item_id] <= 0:
-                del self.items[item_id]
+    def remove_item(self, item, quantity=1):
+        if item in self.items:
+            self.items[item] -= quantity
+            if self.items[item] <= 0:
+                del self.items[item]
 
     def get_items(self):
-        return {self.price_list[item_id]: qty for item_id, qty in self.items.items()}
+        return self.items
 
 
 if __name__ == "__main__":
     shopping_list = ShoppingList()
-    shopping_list.add_item("APPL", 1)
-    shopping_list.add_item("BANA", 2)
-    shopping_list.add_item("BOOST", 2)  
-    print("Efter tilføjelse(r):", shopping_list.get_items())
-    shopping_list.remove_item("APPL")
-    print("Fjernet æble(r):", shopping_list.get_items()) 
-    shopping_list.remove_item("BANA", 1)
-    print("Fjernet banan(er):", shopping_list.get_items())
-    shopping_list.remove_item("BOOST", 2)
-    print("Removed booster(s):", shopping_list.get_items())
+    shopping_list.add_item("Apples", 1)
+    shopping_list.add_item("Bananas", 2)
+    shopping_list.add_item("Boosters", 2)  
+    print("Efter tilføjelser:", shopping_list.get_items())
+    
+    shopping_list.remove_item("Apples")
+    print("fjernet æble(r):", shopping_list.get_items())
+    
+    shopping_list.remove_item("Bananas", 1)
+    print("fjernet banan(er):", shopping_list.get_items())
+    
+    shopping_list.remove_item("Boosters", 2)
+    print("removed booster(s):", shopping_list.get_items())
